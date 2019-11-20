183e48a2ac23aca620429153f1a9c2d3f479733a
nekojess1 <jessi_alves1@outlook.com>
2019-11-20 17:59:48 -0300

    Adicionando uma trigger e uma view

---
+++ functions/salvaexcluido.sql
@@ -0,0 +1,10 @@
+﻿CREATE OR REPLACE FUNCTION salvaexcluido()
+RETURNS trigger AS $$
+BEGIN 
+INSERT INTO bk_cliente values(old.Nome, old.Endereco, old.rg, old.cidade, old.estado);
+
+RETURN NULL;
+END;
+$$
+LANGUAGE plpgsql;
+
---
+++ views/validade_e_produto.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW validade_e_produto as
+SELECT descricao, quantidade, valor,
+(valor*quantidade) as valor_total, 
+validade, categoria_produto.nome as
+categorias, ehvalido(produtos.id)
+from produtos 
+inner join categoria_produto on produtos.categoria_id = categoria_produto.id;
+
+
