81525022898828681223417bb5021b3019341358
nekojess1 <jessi_alves1@outlook.com>
2019-11-21 13:55:49 -0300

    Alterações básicas de estrutra

--- functions/salvaexcluido.sql
+++ functions/salvaexcluido.sql
@@ -2,7 +2,7 @@
 RETURNS trigger AS $$
 BEGIN 
 INSERT INTO bk_cliente values(old.Nome, old.Endereco, old.rg, old.cidade, old.estado);
-
+RAISE NOTICE 'Backup realizado com sucesso;';
 RETURN NULL;
 END;
 $$
---
+++ triggers/salvar_apos_excluir.sql
@@ -0,0 +1,7 @@
+﻿CREATE TRIGGER exclusao_cliente
+AFTER DELETE    		    -- Sera executado depois de deletar
+on cliente      		    -- Na tabela cliente             
+FOR EACH ROW			    -- Vai executar para cada linha dentro da tabela
+EXECUTE 
+PROCEDURE salvaexcluido();  -- Executar esse procedimento.
+
